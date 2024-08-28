83dc8da44865083e85e9c3d8329a5b3a4e937fbb
Sagarpatil2003 <sp4765699@gmail.com>
2024-08-28 23:28:14 +0530

    start project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import Nav from "./Nav";
+
+function App() {
+  
+  
+
+ 
+  
+ 
+  return (
+    <div >
+      <Nav/>
+    </div>
+  );
+}
+
+export default App;
